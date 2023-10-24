318a03cada6f5095a86847ff0d1faf5ae4d1d2fd
hardik2901 <vatshardik@yahoo.com>
2023-10-25 03:23:27 +0530

    finalDone

--- amazon-frontend/src/pages/Admin/AddProduct.js
+++ amazon-frontend/src/pages/Admin/AddProduct.js
@@ -1,81 +1,15 @@
 import { useDispatch } from "react-redux";
 import "../../styles/AddProduct.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { addProduct } from "../../actions/ProdcutActions";
+import {
+  useAddress,
+  useContract,
+  useMetamask,
+  useContractWrite,
+} from "@thirdweb-dev/react";
 
 const AddProduct = () => {
-  const { contract } = useContract(
-    "0xA85D1Ae5157E6c6eEd906cDd20800f753a15429a"
-  );
-
-  const address = useAddress();
-  const connect = useMetamask();
-
-  const [form, setForm] = useState({
-    CarbonEmission: "",
-    WaterUsage: "",
-    EnergyEfficiency: "",
-    WasteGeneration: "",
-    RenewableEnergyUsage: "",
-    BiodiversityUsage: "",
-  });
-
-  const { mutateAsync: addProduct } = useContractWrite(contract, "addProduct");
-  const [isLoading, setIsLoading] = useState(false);
-  const [qrData, setQrData] = useState(null);
-  const [url, setUrl] = useState("");
-
-  function onClickHandler() {
-    console.log("hello");
-    connect();
-  }
-  useEffect(() => {
-    if (qrData) {
-      generateQR(qrData);
-    }
-  }, [qrData]);
-
-  const createnewItem = async (form) => {
-    const data = await addProduct({
-      args: [
-        form.CarbonEmission,
-        form.WaterUsage,
-        form.EnergyEfficiency,
-        form.WasteGeneration,
-        form.RenewableEnergyUsage,
-        form.BiodiversityUsage,
-      ],
-    });
-
-    return data;
-  };
-
-  const handleFormFieldChange = (fieldName, e) => {
-    setForm({ ...form, [fieldName]: e.target.value });
-  };
-
-  const generateQR = async () => {
-    try {
-      const response = await QrCode.toDataURL(qrData);
-      setUrl(response);
-    } catch (error) {
-      console.error("Error", error);
-    }
-  };
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    setIsLoading(true);
-    const data = await createnewItem({
-      ...form,
-    });
-    console.log(data);
-    const num = data.receipt.events[0].args[0];
-    console.log(num);
-    const numAsString = num.toString();
-    setQrData(numAsString);
-    setIsLoading(false);
-  };
-
   const [product, setProduct] = useState({});
   const dispatch = useDispatch();
 
@@ -84,10 +18,56 @@ const AddProduct = () => {
     dispatch(addProduct(product));
   };
 
+  const { contract } = useContract(
+    "0xA85D1Ae5157E6c6eEd906cDd20800f753a15429a"
+  );
+
   const handleOnChange = (e) => {
     setProduct((prev) => {
       return { ...prev, [e.target.name]: e.target.value };
     });
+  };
+
+  function calculateScore(productData, weights) {
+    let score = 0;
+
+    for (const factor in weights) {
+      if (
+        weights.hasOwnProperty(factor) &&
+        productData.hasOwnProperty(factor)
+      ) {
+        score += productData[factor] * weights[factor];
+      }
+    }
+
+    const maxPossibleScore = 1000;
+    const normalizedScore = (score / maxPossibleScore) * 100;
+
+    return normalizedScore;
+  }
+
+  let productData = {};
+
+  const weights = {
+    carbon_emissions: 30,
+    water_usage: 20,
+    energy_efficiency: 10,
+    waste_generation: 25,
+    renewable_energy_usage: 10,
+  };
+
+  const getM = async () => {
+    const pId = 3;
+    const metric = await contract.call("getProduct", [pId]);
+
+    productData.carbon_emissions = parseInt(metric[1]);
+    productData.water_usage = parseInt(metric[2]);
+    productData.energy_efficiency = parseInt(metric[3]);
+    productData.waste_generation = parseInt(metric[4]);
+    productData.renewable_energy_usage = parseInt(metric[5]);
+
+    const score = calculateScore(productData, weights);
+    console.log(score);
   };
 
   return (
@@ -154,77 +134,8 @@ const AddProduct = () => {
         <label>Production Metrics</label>
         <input type="file" accept=".json" />
       </form>
-      <div className="bcn-container">
-        {isLoading && <Loader />}
-        <form onSubmit={handleSubmit} className="bcn-form">
-          <div className="formFields">
-            <FormField
-              labelName="Carbon Emission"
-              inputType="number"
-              value={form.CarbonEmission}
-              handleChange={(e) => handleFormFieldChange("CarbonEmission", e)}
-            />
-
-            <FormField
-              labelName="Energy Efficicency"
-              inputType="number"
-              value={form.EnergyEfficiency}
-              handleChange={(e) => handleFormFieldChange("EnergyEfficiency", e)}
-            />
-
-            <FormField
-              labelName="Water Usage"
-              inputType="number"
-              value={form.WaterUsage}
-              handleChange={(e) => handleFormFieldChange("WaterUsage", e)}
-            />
-
-            <FormField
-              labelName="Renewable Energy Usage"
-              inputType="number"
-              value={form.RenewableEnergyUsage}
-              handleChange={(e) =>
-                handleFormFieldChange("RenewableEnergyUsage", e)
-              }
-            />
-            <FormField
-              labelName="Waste Generation"
-              inputType="number"
-              value={form.WasteGeneration}
-              handleChange={(e) => handleFormFieldChange("WasteGeneration", e)}
-            />
-            <FormField
-              labelName="Biodiversity Usage"
-              inputType="text"
-              value={form.BiodiversityUsage}
-              handleChange={(e) =>
-                handleFormFieldChange("BiodiversityUsage", e)
-              }
-            />
-            <div className="bcn-buttons">
-              <button
-                type="button"
-                onClick={onClickHandler}
-                className="connect-button"
-              >
-                Connect
-              </button>
-              <button className="bcn-submit" type="sumbit">
-                Register
-              </button>
-            </div>
-          </div>
-        </form>
-        {qrData && (
-          <div className="qrCode">
-            <span className="f">Product Registered Successfully </span>
-            <div className="">
-              <img src={url} alt="qrcode" />
-            </div>
-          </div>
-        )}
-      </div>
       <button onClick={submit}>Submit</button>
+      <button onClick={getM}>GET</button>
     </div>
   );
 };
